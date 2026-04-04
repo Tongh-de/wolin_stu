4d799aaf6a4b4af1002f4f1af91720df0702811c
morfean <16909269+morfean@user.noreply.gitee.com>
2026-04-04 17:40:12 +0800

    modified exam_dao.py by morfean

--- dao/exam_dao.py
+++ dao/exam_dao.py
@@ -1,7 +1,6 @@
 from sqlalchemy import and_
 from sqlalchemy.orm import Session
 from model.exam_model import StuExamRecord
-from typing import Optional
 
 
 # 按考核序次录⼊考核成绩（学⽣编号、考核序次、成绩）
@@ -62,22 +61,18 @@ def exam_delete(
         seq_no: int | None,
         db: Session
 ):
-    # 取对应考试序次/所有考试序次的数据
+    # 先构建基础查询(不执行)
+    _query = db.query(StuExamRecord).filter(
+        and_(
+            StuExamRecord.is_deleted == 0,
+            StuExamRecord.stu_id == stu_id
+        )
+    )
+    # 判断seq_no的传入值再执行
     if seq_no is not None:
-        _query = db.query(StuExamRecord).filter(
-            and_(
-                StuExamRecord.is_deleted == 0,
-                StuExamRecord.stu_id == stu_id,
-                StuExamRecord.seq_no == seq_no
-            )
-        ).all
+        _query = _query.filter(StuExamRecord.seq_no == seq_no).first()
     else:
-        _query = db.query(StuExamRecord).filter(
-            and_(
-                StuExamRecord.is_deleted == 0,
-                StuExamRecord.stu_id == stu_id
-            )
-        ).all
+        _query = _query.all()
     # 逻辑删除查询到的数据
     if not _query:
         return f"'{stu_id=}' & '{seq_no=}' not found"
